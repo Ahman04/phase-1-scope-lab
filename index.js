a14e359b1df8571448111dc7fdb9f2e41d717035
Ahman04 <abokey2020@gmail.com>
2025-09-08 14:31:01 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+// 1. Declare global variable
+var customerName = "bob";
+
+// 2. Function that uppercases global variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Declare bestCustomer 
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// 4. Overwrite bestCustomer
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+// 5. Declare constant leastFavoriteCustomer
+const leastFavoriteCustomer = "initial value";
+
+function changeLeastFavoriteCustomer() {
+ 
+  leastFavoriteCustomer = "new value"; // This will throw an error when called
+}
+
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  bestCustomer,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer
+};
